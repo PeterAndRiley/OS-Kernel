4e8af4a729da330fcc2806b737d5b7942b493f18
Peterlits Zo <peterlitszo@outlook.com>
2021-01-05 12:30:13 +0800

    feat: (src/main) smallest no_std && no_main source
    The OS entry cannot need `std` and `main` because those need another OS
    to suppost.
    
    Instead of `main`, there is a function named `_start`, which is the
    entry point of this project. Because the linker looks for this function
    named `_start` by default.
    
    To deal with panic, there is a function `panic` to try to deal with it.
    
    To deal with abort, there is a function `abort` to try to deal with it.
    
    And it also need an language item, so there is a `eh_personality` here.
    
    for more infomation, click:
        https://intermezzos.github.io/book/first-edition/creating-our-first-crate.html
        (zh) https://learningos.github.io/rcore_step_by_step_webdoc/docs/%E7%8B%AC%E7%AB%8B%E5%BC%8F%E5%8F%AF%E6%89%A7%E8%A1%8C%E7%A8%8B%E5%BA%8F.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(lang_items)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// TODO: write the comment
+//       Peterlits<peterlitszo@outlook.com>
+#[no_mangle]
+pub extern fn abort () {
+    panic!("abort!");
 }
+
+// TODO: write the comment
+//       Peterlits<peterlitszo@outlook.com>
+#[lang = "eh_personality"]
+extern fn eh_personality () {
+}
+
+// TODO: write the comment
+//       Peterlits<peterlitszo@outlook.com>
+#[panic_handler]
+fn panic (_info: &PanicInfo) -> ! {
+    loop { }
+}
+
+// this function is the *entry point*, since the linker looks for a function
+// named `_start` by default.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
