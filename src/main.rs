b3b87dd3871519fd20ea4f70e827bc5879c890a1
Peterlits Zo <peterlitszo@outlook.com>
2021-01-10 01:20:39 +0800

    feat: (src/main, *) change the entry function in asm.
    The origin entry function named `_start` has moved to
    `./src/boot/entry64.asm`, which will call `rust_main`.
    
    By the way, there is a new function called `console_putchar` which will
    put a character on the console.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-#![feature(lang_items)]
 #![no_std]
 #![no_main]
+#![feature(llvm_asm)]
+#![feature(global_asm)]
 
 use core::panic::PanicInfo;
 
 // TODO: write the comment
 //       Peterlits<peterlitszo@outlook.com>
 #[no_mangle]
-pub extern fn abort () {
+extern "C" fn abort () -> ! {
     panic!("abort!");
 }
 
@@ -18,10 +19,31 @@ fn panic (_info: &PanicInfo) -> ! {
     loop { }
 }
 
+global_asm!(include_str!("boot/entry64.asm"));
+
+pub fn console_putchar(ch: u8) {
+    let ret: usize;
+    let arg0: usize = ch as usize;
+    let arg1: usize = 0;
+    let arg2: usize = 0;
+    let which: usize = 1;
+    unsafe {
+        llvm_asm!("ecall"
+             : "={x10}" (ret)
+             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+             : "memory"
+             : "volatile"
+        );
+    }
+}
+
 // this function is the *entry point*, since the linker looks for a function
 // named `_start` by default.
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn rust_main() -> ! {
+    console_putchar(b'O');
+    console_putchar(b'K');
+    console_putchar(b'\n');
     loop {}
 }
 
