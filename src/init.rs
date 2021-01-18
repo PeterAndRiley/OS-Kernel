3531652b5375d95eb77c68b70f1193dae907b314
Peterlits Zo <peterlitszo@outlook.com>
2021-01-18 12:40:55 +0800

    docs: (src/timer.rs, *) add `timer` to raise `timer's interrupt`.
    `timer's interrupt` is important for an OS Kernel. To make it, I add a
    file `src/timer.rs`:
    
        - `src/timer.rs`
            `init` function will support for initialzation of RISC-V's
            timer, which will set the timer after `TIMEBASE` again and
            again, and also raise a timer's interrupt, which will be dealed
            by `src/interrupt.rs`.
    
    I also change some files:
    
        - `src/init.rs`
            I change it to init timer at the entrypoint of this OS.
        - `src/interrupt.rs`
            I rewrite it with match expression to handle both timer's
            interrupt and the breakpoint exception.
    
                - `set_sum`: Permit Supervisor User Memory access
                - `set_sie`: Supervisor Interrupt Enable
                More info ->
                https://docs.rs/crate/riscv/0.6.0/source/src/register/sstatus.rs
    
        - `src/lib.rs`
            add line: `mod timer;`
        - `src/sbi.rs`
            add function `set_timer`, which use asm `ecall` to set time in
            RISC-V.

--- src/init.rs
+++ src/init.rs
@@ -1,12 +1,11 @@
 global_asm!(include_str!("boot/entry64.asm"));
 
-use crate::interrupt::init;
-
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
     crate::interrupt::init();
+    crate::timer::init();
     unsafe {
         llvm_asm!("ebreak"::::"volatile");
     }
-    panic!("end of rust_main");
+    loop {}
 }
--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,11 +1,20 @@
 use riscv::register::{
-    scause,
+    scause::{
+        self,
+        Trap,
+        Exception,
+        Interrupt
+    },
     sepc,
     stvec,
     sscratch,
     sstatus
 };
 use crate::context::TrapFrame;
+use crate::timer::{
+    TICKS,
+    clock_set_next_event
+};
 
 global_asm!(include_str!("trap/trap.asm"));
 
@@ -17,15 +26,32 @@ pub fn init() {
         sscratch::write(0);
         stvec::write(__alltraps as usize, stvec::TrapMode::Direct);
         sstatus::set_sum();
+        sstatus::set_sie();
     }
     println!("++++ setup interrupt! ++++");
 }
 
 #[no_mangle]
 pub fn rust_trap(tf: &mut TrapFrame) {
-    let cause = scause::read().cause();
-    let epc = sepc::read();
-    println!("trap: cause: {:?}, epc: {:?}", cause, epc);
+    match tf.scause.cause() {
+        Trap::Exception(Exception::Breakpoint) => breakpoint(&mut tf.sepc),
+        Trap::Interrupt(Interrupt::SupervisorTimer) => super_timer(),
+        _ => panic!("undefined trap!")
+    }
+}
 
-    tf.sepc += 2;
+fn breakpoint(sepc: &mut usize) {
+    println!("a breakpoint set @0x{:x}", sepc);
+    *sepc += 2;
+}
+
+fn super_timer() {
+    clock_set_next_event();
+    unsafe {
+        TICKS += 1;
+        if (TICKS == 100) {
+            TICKS = 0;
+            println!("* 100 ticks *");
+        }
+    }
 }
