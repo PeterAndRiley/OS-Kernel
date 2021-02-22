493c191c1ea69df4d973b7ed57bcecd5a0d63e0b
Peterlits Zo <peterlitszo@outlook.com>
2021-02-22 19:49:34 +0800

    feat: (src/init.rs) add `test_heap`
    I changes files:
    
        - `src/init.rs`, add function `test_heap` and test when the kernel is
          booted. Function `test_heap` is based on `src/memory`, which has a
          static array as a heap.

--- src/init.rs
+++ src/init.rs
@@ -1,3 +1,5 @@
+extern crate alloc;
+
 use crate::consts::*;
 
 global_asm!(include_str!("boot/entry64.asm"));
@@ -17,8 +19,28 @@ pub extern "C" fn rust_main() -> ! {
         PHYSICAL_MEMORY_END >> 12
     );
     crate::memory::init_heap();
+    test_heap();
+    
     unsafe {
         llvm_asm!("ebreak"::::"volatile");
     }
     loop {}
+}
+
+fn test_heap() {
+    use alloc::vec::Vec;
+    use alloc::boxed::Box;
+    println!("test for heap...");
+    let v = Box::new(5);
+    assert!(*v == 5);
+    core::mem::drop(v);
+    let mut vec = Vec::new();
+    for i in 0..10000 {
+        vec.push(i);
+    }
+    assert!(vec.len() == 10000);
+    for (i, value) in vec.into_iter().enumerate() {
+        assert_eq!(value, i);
+    }
+    println!("test for heap passed");
 }
